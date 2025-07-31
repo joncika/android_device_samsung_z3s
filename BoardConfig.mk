854cfa4707d991cc2c8e251dc862f8d00798fafc
joncika <jonconerd@gmail.com>
2025-07-31 12:09:47 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,14 @@
-# Copyright (C) 2024 The TWRP Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # 12.1 Manifest Requirements
 ALLOW_MISSING_DEPENDENCIES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
@@ -61,12 +56,14 @@ BOARD_ROOT_EXTRA_FOLDERS := \
     efs \
     keydata \
     keyrefuge \
-    linkerconfig \
     metadata \
     omr \
     optics \
     prism \
     spu
+
+# Include
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
